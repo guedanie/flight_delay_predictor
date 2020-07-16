16ba275e9b728e4ba6123d3cb38887a10764f6c1
guedanie <danguerrero93@gmail.com>
2020-07-16 10:45:10 -0500

    added new functions for weather data

--- preprocessing.py
+++ preprocessing.py
@@ -28,6 +28,8 @@ def to_date_time(df):
     df.fl_date = pd.to_datetime(df.fl_date)
     df["day_of_week"] = df.fl_date.dt.dayofweek
     df["month"] = df.fl_date.dt.month
+    df["avg_month_delay"] = df.groupby("month").arr_delay.transform("mean")
+    df["avg_day_of_week_delay"] = df.groupby("day_of_week").arr_delay.transform("mean")
     return df
 
 def create_new_features(df):
@@ -105,4 +107,149 @@ def mvp_modeling_prep(modeling = False, features_for_modeling=[], target_variabl
         return train_scaled, y_train, validate_scaled, y_validate, test_scaled, y_test
 
 
+#############################################################################
+#                       Weather data preprocessing                          #
+#############################################################################
 
+
+def prep_flight_data_weather():
+    df = wrangle.prep_flight_data()
+    
+    df["crs_dep_time"] = df['crs_dep_time'].astype(str).apply(lambda x: x.zfill(4))
+    df["fl_datetime"] = df.fl_date.astype(str) + " " + df.crs_dep_time.astype(str)
+    df.fl_datetime = pd.to_datetime(df.fl_datetime, format="%Y-%m-%d %H%M")
+    df = df.set_index("fl_datetime")
+    df.index = df.index.map(lambda t: t.replace(minute=30))
+
+    features_to_keep = ["fl_date", "op_carrier", "op_carrier_fl_num", "crs_dep_time", "origin", "dest", "dep_delay", "arr_delay", "weather_delay", "Airline"]
+    df = df[features_to_keep]
+    df = df.reset_index()
+
+    df["unique_id"] = df.fl_datetime.astype(str) + "_" + df.origin
+
+    df = df.sort_values(["origin", "fl_datetime", "op_carrier"])
+
+    return df
+
+
+def prep_weather_data():
+    weather_data = pd.read_csv("US_WeatherEvents_2016-2019.csv")
+    
+    # we only wand data for the following cities
+    top_airports = ["ATL", "LAX", "ORD", "DFW", "DEN", "JFK", "SFO", "SEA", "LAS", "MCO", "EWR", "CLT", "PHX", "IAH", "MIA"]
+    
+    airport_codes = pd.read_csv("https://storage.googleapis.com/kagglesdsdata/datasets%2F626214%2F1116273%2Fairports.csv?GoogleAccessId=gcp-kaggle-com@kaggle-161607.iam.gserviceaccount.com&Expires=1594952131&Signature=CbuLdZR%2B9YkSTVhPslvV67WILvcTGa1EnhtSpZza10YkL2ccV6HIZLkikKrkF%2BEgwFySBM88hZ4Gp9aqZqA5s5BC%2FL7l71dpdyM8PkBWxtmvJkEYuPYxPvzwt2M10q7T4TzK1mFzUUwPz1CtvLcBdx%2F3GJgd5z4nSGepU3SkoalQHJ4JUQ0SpE5liZiCJ2SB%2FTA4fcz62TvAKv21Jwe7oq4q8CnrG6rvSjW5uRDkvKXDc1sXH2WG2CIZYyt%2FKz2B%2BJsi2iZ9AS9dAhbLO8GVOP6EhtIA9%2ForRmxuBStdXY%2BY1lojJaekb9agtbNS5wAIr%2Fkk0LUHmKPxt7b6jTiLtQ%3D%3D")
+    airport_codes = airport_codes[airport_codes.Country == "United States"]
+
+    weather_data = weather_data.merge(airport_codes, how="left", left_on="AirportCode", right_on="ICAO")
+
+    top_airports = ["ATL", "LAX", "ORD", "DFW", "DEN", "JFK", "SFO", "SEA", "LAS", "MCO", "EWR", "CLT", "PHX", "IAH", "MIA"]
+
+    for i in top_airports:
+        weather_data.loc[weather_data['IATA'] == i, 'is_top'] = True 
+        
+    weather_data.is_top = weather_data.is_top.fillna(False)
+    weather_data = weather_data[weather_data.is_top]
+    
+    weather_data["StartTime(UTC)"] = pd.to_datetime(weather_data["StartTime(UTC)"])
+
+    weather_data = weather_data.set_index("StartTime(UTC)")
+
+    weather_data = weather_data["2018"]
+
+    return weather_data
+
+def filter_weather_data(weather_data):
+    
+    weather = pd.DataFrame()
+    unique_cities = weather_data.City_y.unique()
+    # Given that not all cities have the same starting point, we need to basically individually filter 
+    # for each city, reset their index and impude the missing values. The biggest issue is the fact that
+    # only precipitation data is reported, so normal weather conditions are not present. As 
+    # such we inpude them here
+    for city in unique_cities:
+        city_data = weather_data[weather_data.City_y == city]
+        r = pd.date_range(start=city_data.index.min(), end=city_data.index.max(), freq="H")
+        city_data = city_data.reindex(r, copy=True)
+        city_data["EndTime(UTC)"] = city_data["EndTime(UTC)"].fillna(method="ffill")
+        for i in range(city_data["EndTime(UTC)"].unique().shape[0]):
+            date = city_data["EndTime(UTC)"].unique()[i]
+            mask = (city_data["EndTime(UTC)"] == date) & (city_data.index >= date)
+            city_data["Type"] = city_data["Type"].mask(mask, city_data["Type"].fillna("Clear"))
+        mask = city_data.Type != "Clear"
+        city_data = city_data.mask(mask, city_data.fillna(method="ffill"))
+        city_data = city_data.fillna(method="ffill")  
+        
+        weather = pd.concat([weather, city_data])
+
+        features_to_keep = ["Type", "Severity", "TimeZone", "IATA", "City_y", "State"]
+
+    weather = weather[features_to_keep]
+
+    weather = weather.sort_values(["City_y", "State"])
+
+    weather.rename(columns={"City_y":"city"}, inplace=True)
+
+    # We will now normalize the data by changing all time stamps to :30 minutes, to makes it easier to merge
+    # with our flights df 
+
+    weather.index = weather.index.map(lambda t: t.replace(minute=30))
+
+    time_zones = weather.TimeZone.unique()
+    weather_df = pd.DataFrame()
+    for zone in time_zones:
+        mask = weather.TimeZone == zone
+        time_zone_df = weather[mask].tz_localize('utc').tz_convert(zone).tz_localize(None)
+        weather_df = pd.concat([weather_df, time_zone_df])
+
+    weather_df["unique_id"] = weather_df.index.astype(str) + "_" + weather_df.IATA
+    weather_df = weather_df.reset_index().rename(columns={"index": "date"})
+    
+    return weather_df
+
+def merge_flight_weather_data():
+    df = prep_flight_data_weather()
+
+    weather_data = prep_weather_data()
+
+    weather_data = filter_weather_data(weather_data)
+
+    merged_df = df.merge(weather_data[["Type", "Severity", "unique_id"]], how="left", right_on="unique_id", left_on="unique_id")
+
+    merged_df.Type = merged_df.Type.fillna("Clear")
+    merged_df.Severity = merged_df.Severity.fillna("Light")
+
+    return merged_df
+
+def weather_modeling_prep(modeling=False, features_for_modeling=[], target_variable=[]):
+    merged_df = merge_flight_weather_data()
+    merged_df = to_date_time(merged_df)
+    merged_df = create_new_features(merged_df)
+    merged_df = create_target_variable(merged_df)
+    
+    # add weather features
+    merged_df["avg_weather_delay"] = merged_df.groupby("Type").arr_delay.transform("mean")
+
+    if modeling == False:
+        return merged_df
+
+    else:
+        
+        features_for_modeling += ["observation"]
+        features_for_modeling += [target_variable]
+
+        merged_df = merged_df[features_for_modeling]
+        merged_df = merged_df.set_index("observation")
+
+        train, validate, test = split_data(merged_df)
+
+        X_train = train.drop(columns=target_variable)
+        y_train = train[target_variable]
+        X_validate = validate.drop(columns=target_variable)
+        y_validate = validate[target_variable]
+        X_test = test.drop(columns=target_variable)
+        y_test = test[target_variable]
+
+        scaler, train_scaled, validate_scaled, test_scaled = min_max_scaler(X_train, X_validate, X_test)
+
+        return train_scaled, y_train, validate_scaled, y_validate, test_scaled, y_test
