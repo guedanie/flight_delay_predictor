c82f8e6f813bc5a87694e6052c0966b0bd352b27
guedanie <danguerrero93@gmail.com>
2020-07-17 12:54:24 -0500

    added new functions for exploration graphs

--- explore.py
+++ explore.py
@@ -51,3 +51,69 @@ def delay_by_severity(weather_df):
         
     plt.show()
             
+
+def data_distribution(df):
+    # Is arr delay evenly distributed?
+
+    plt.figure(figsize=(15,5))
+    sns.distplot(df.arr_delay)
+    plt.title("Is arrival delays evenly distributed?")
+    plt.ylabel("Frequency")
+    plt.xlabel("Minutes Delayed")
+
+def delay_by_airline_minutes(df):
+    # Delays in minutes
+    df.groupby("op_carrier").arr_delay.mean().sort_values().plot.bar(figsize=(15,5))
+    plt.title("Which airline is delayed the most?")
+    plt.ylabel("Minutes Delayed")
+    plt.xlabel("Airline Code")
+
+def delay_by_airline_flights(df):
+    # Delays by flights (count)
+
+    df[df.arr_delay > 0].groupby("op_carrier").arr_delay.count().sort_values().plot.bar(figsize=(15,5))
+    plt.title("Which airline has the most delays?")
+    plt.ylabel("Number of Flights")
+    plt.xlabel("Airline Code")
+
+def delay_by_airport_minutes(df):
+    # Delays in minutes
+    df.groupby("origin").arr_delay.mean().sort_values().plot.bar(figsize=(15,5))
+    plt.title("Which airport experiences the most delays, in minutes?")
+    plt.ylabel("Minutes Delayed")
+    plt.xlabel("Airline Code")
+
+def delay_by_airport_flights(df):
+    df[df.arr_delay > 0].groupby("origin").arr_delay.count().sort_values().plot.bar(figsize=(15,5))
+    plt.title("Which airport has the most number of flights delayed?")
+    plt.ylabel("Number of Flights")
+    plt.xlabel("Airline Code")
+
+def most_common_reason_for_delay(df):
+    df.iloc[:,12:17].mean().sort_values().plot.bar(figsize=(15,5))
+    plt.title("What is the most common reason for a delay?")
+    plt.ylabel("Avg minutes delayed")
+    plt.xlabel("Reason for delay")
+
+def delays_by_destination(df):
+    top_airports = ["ATL", "LAX", "ORD", "DFW", "DEN", "JFK", "SFO", "SEA", "LAS", "MCO", "EWR", "CLT", "PHX", "IAH", "MIA"]
+    for i in top_airports:
+        df.loc[df['dest'] == i, 'is_top'] = True
+    
+    df.is_top = df.is_top.fillna(False)
+
+    print(f"Top airport's had an average of {df[df.is_top].arr_delay.mean():.0f} minutes when flying to other top airports")
+    print(f"Top airport's had an average of {df[~df.is_top].arr_delay.mean():.0f} minutes when flying to non-top airports")
+
+def destination_delay_statistical_testing(df):
+    subgroup_1 = df[df.is_top].arr_delay
+    subgroup_2 = df[~df.is_top].arr_delay
+
+    tstat, p = stats.ttest_ind(subgroup_1, subgroup_2)
+
+    print(f"p value: {p:.2f}")
+
+# ------------------- #
+#     Time Series     #
+# ------------------- #
+
